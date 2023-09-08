95d3d34e4827ac13fdf96b4c7128b01723cc967b
elijahthis <emperorelijah007@gmail.com>
2023-09-08 04:41:46 +0100

    fix: fixed mobile overflow

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -144,7 +144,7 @@ const Home = () => {
 							</div>
 						</div>
 					</div>
-					<div className="relative">
+					<div className="relative hidden lg:block ">
 						<Modal open={whichModal !== null}>{renderModal()}</Modal>
 					</div>
 				</div>
